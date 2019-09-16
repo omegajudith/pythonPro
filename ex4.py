ec93875d9723dfc9befd871e5db6a9d594807e8d
omegajudith <omegajudith@gmail.com>
2019-09-16 16:22:01 +0300

    py exercise

--- ex4.py
+++ ex4.py
@@ -1,32 +1,4 @@
-# grade = input("Enter your grade: ")
-# if grade >= 90:
-#   print("A")
-# elif grade >= 80:
-#   print("B")
-# elif grade >= 70:
-#   print("C")
-# elif grade >= 65:
-#   print("D")
-# else:
-#   print("F")
 
-# list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-# item = []
-# for elements in list:
-# if elements < 5:
-# print(item)
-
-#prints an empty list
-# list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-# item = []
-# for elements in list:
-#     if elements < 5:
-#          print(item)
-
-# a = []
-# 
-#     if nums < 5:
-#         print([nums])
 
 list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89] 
 
@@ -37,13 +9,4 @@ for nums in list:
         # print([nums])
         print(nums)
 
-
-
-# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-# x=a
-# for x in a: 
-# (i ran this code,Showing syntax error, at if ,)if x <= 5:
-# print(f'this value of this index is less than 5 and it is {x}')
-# else:
-# pass
 
